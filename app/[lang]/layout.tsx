5078dafa5931d869e958cf34d33faddffff4b231
sergio-flow <sergio.finciuc@gmail.com>
2025-04-03 09:12:44 +0300

    Added G tags

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import "@/node_modules/flag-icons/css/flag-icons.min.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "English Segments",
@@ -14,6 +15,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <head>
+        <link rel="stylesheet" href="https://emoji-css.afeld.me/emoji.css" />
+        {/* <meta name="google-adsense-account" content="ca-pub-3923017048696264" />
+        <Script id="adsense" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3352560740569043"
+          crossOrigin="anonymous"></Script> */}
+
+        <Script id="gtag" async src="https://www.googletagmanager.com/gtag/js?id=G-ZVCTZC0EQH"></Script>
+        <Script id="gconfig">{`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-ZVCTZC0EQH');        
+`}</Script>
+      </head>
       <body>
         {children}
       </body>
