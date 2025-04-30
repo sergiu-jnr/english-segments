a178e880eeaccfe529c79d326faf9bf824daab0c
Sergio <sergio.finciuc@gmail.com>
2025-04-30 13:28:30 +0300

    Update page.tsx

--- app/[lang]/blog/page.tsx
+++ app/[lang]/blog/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params }: {
   }
 }
 
-export const revalidate = 7884000
+export const revalidate = 1209600
 
 export default async function Blog({ params }: {
   params: Promise<{ lang: Lang }>
