efd73f488881d28d3332123100ecb42fa3fd36fa
Sergio <sergio.finciuc@gmail.com>
2025-05-01 10:18:43 +0300

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -35,13 +35,13 @@ export default async function RootLayout({
     <html lang={lang}>
       <head>
         <link rel="stylesheet" href="https://emoji-css.afeld.me/emoji.css" />
-        <link rel="icon" type="image/png" href="/media/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/media/favicon.svg" />
-        <link rel="icon" href="https://www.englishsegments.com/favicon.ico" type="image/x-icon">
-        <link rel="shortcut icon" href="/media/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/media/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="https://www.englishsegments.com/media/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="https://www.englishsegments.com/media/favicon.svg" />
+        <link rel="icon" href="https://www.englishsegments.com/media/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="https://www.englishsegments.com/media/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://www.englishsegments.com/media/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-title" content="English" />
-        <link rel="manifest" href="/media/site.webmanifest" />
+        <link rel="manifest" href="https://www.englishsegments.com/media/site.webmanifest" />
         {/* <meta name="google-adsense-account" content="ca-pub-3923017048696264" />
         <Script id="adsense" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3352560740569043"
           crossOrigin="anonymous"></Script> */}
